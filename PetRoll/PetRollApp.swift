8ccccfe63148843d70313c240af143158ba3134d
fathurhdytt <fathurhidayat2003@gmail.com>
2025-03-26 16:36:33 +0700

    Initial Commit

---
+++ PetRoll/PetRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetRollApp.swift
+//  PetRoll
+//
+//  Created by Muhammad Fathur Hidayat on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PetRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetRollTests/PetRollTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetRollTests.swift
+//  PetRollTests
+//
+//  Created by Muhammad Fathur Hidayat on 26/03/25.
+//
+
+import Testing
+@testable import PetRoll
+
+struct PetRollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
